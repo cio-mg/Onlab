a55737cd5b553e9ff32cd3eb38ea605631a99283
Cio-mg <ciomg78@hotmail.com>
2021-11-24 22:27:04 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
+import { NavbarComponent } from './navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ServicioComponent } from './servicio/servicio.component';
+import { PacienteComponent } from './paciente/paciente.component';
+import { PreguntasComponent } from './preguntas/preguntas.component';
+import { AboutComponent } from './about/about.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServicioComponent,
+    PacienteComponent,
+    PreguntasComponent,
+    AboutComponent,
+    RegistrarseComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
